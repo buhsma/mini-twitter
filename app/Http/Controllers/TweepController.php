2b7276b9a8f859427304226fa3a6e603aaf94c9b
buhsma <buhsma@gmail.com>
2024-03-22 16:22:40 +0100

    TweepController, googleroute

---
+++ app/Http/Controllers/TweepController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TweepController extends Controller
+{
+    public function handleGoogleSignIn(Request $request)
+    {
+        $response = $request->input('response');
+
+        if (isset($response['error'])) {
+            // Handle errors (e.g., user cancellation, network issues)
+            return response()->json(['error' => $response['error']], 400);
+        } else {
+            // Access user profile information
+            $accessToken = $response['credential'];
+
+            // Use the access token to retrieve user profile data
+            $userData = Http::withHeaders([
+                'Authorization' => 'Bearer ' . $accessToken,
+            ])->get('https://www.googleapis.com/userinfo/v2/me');
+
+            if ($userData->successful()) {
+                // Extract the desired user profile information (e.g., name, email)
+                $profileData = $userData->json();
+                echo($profileData);// Use the information as needed (e.g., store in database, return to client)
+                return response()->json($profileData);
+            } else {
+                // Handle errors during profile data retrieval
+                return response()->json(['error' => 'Error fetching user profile'], $userData->status());
+            }
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TweetController;
+use App\Http\Controllers\TweepController;
 
 Route::get('/tweets', [TweetController::class, 'index']);
 Route::get('/tweets/{id}', [TweetController::class, 'show']);
@@ -10,6 +11,8 @@ Route::get('/{userId}/tweets', [TweetController::class, 'getTweetsByUserId']);
 Route::post('/tweets', [TweetController::class, 'store']);
 Route::put('/tweets/{id}', [TweetController::class, 'update']);
 Route::delete('/tweets/{id}', [TweetController::class, 'destroy']);
+
+Route::post('/', [TweepController::class, 'googleSignIn']);
 
 Route::get("/test-me", function () {
     return 'Hallo vom Laravel Backend!';
