c8b18b08343422c8d6edafe6899b3e926c80d297
buhsma <buhsma@gmail.com>
2024-03-26 16:04:10 +0100

    debug signin again

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://ynotx.ch'),
+    'url' => env('APP_URL', 'https://localhost'),
 
     'asset_url' => env('ASSET_URL'),
 
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,20 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel Vue</title>
+    <title>Y</title>
     
     @vite(['resources/scss/app.scss', 'resources/js/app.js'])
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
-    
+    <link href="/public/build/assets/app-CehXqgVB.css">
+    <link href="/public/build/assets/app-DLtRB5uD.css">
+    <link href="/public/build/assets/app-fuKTzp_A.js">
     
 </head>
 <body>
-    @if (!session()->has('userId') || !session()->has('userName'))
-        <script src="https://accounts.google.com/gsi/client" async defer></script>
-    @endif
+    
+    <script src="https://accounts.google.com/gsi/client" async defer></script>
+    
     <div id="g_id_onload"
      data-client_id="852116997814-lfcrnrnr0h53blov5g6nlanggpp2spmv.apps.googleusercontent.com"
      data-context="signin"
