b3e85df9adfa2d9ed1ccc5f55c26902c6d8c7925
buhsma <buhsma@gmail.com>
2024-03-26 09:04:12 +0100

    isit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-    base: '/laravel_vue/',
+    base: '/var/www/html/laravel_vue/',
     
     plugins: [
         vue(),
